4c4468690c69160bedb3948199f7e3d0b8660b58
Om Dhameliya <om@example.com>
2025-08-18 10:21:09 +0530

    First commit

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+require('dotenv').config();
+const app = express();
+app.use(express.json());
+
+
+app.listen(process.env.PORT,()=>{console.log(`Server started on PORT : ${process.env.PORT}`)});
