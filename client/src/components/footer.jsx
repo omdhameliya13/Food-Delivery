bb8f0015168ed09ebe8147acdb779b5f8f533645
Praveen Kumar chebrolu <praveenchebrolu18@gmail.com>
2025-09-25 07:43:22 -0700

    Create footer.jsx

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import "../styles/global.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} HomeCook | Fresh Homemade Meals</p>
+    </footer>
+  );
+};
+
+export default Footer;
