af3d27e6ca4e41da52560765a33cf2769e218d48
Praveen Kumar chebrolu <praveenchebrolu18@gmail.com>
2025-09-25 07:47:08 -0700

    Create admindashboard.jsx

---
+++ client/src/pages/admin/admindashboard.jsx
@@ -0,0 +1,10 @@
+const AdminDashboard = () => {
+  return (
+    <div className="dashboard">
+      <h2>Admin Dashboard</h2>
+      <p>Monitor users, chefs, and orders</p>
+    </div>
+  );
+};
+
+export default AdminDashboard;
